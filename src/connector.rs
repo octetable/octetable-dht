4d5f8282ba2842201a64ba4bec8f7df25e428554
Romain Leroux <lerouxrgd@users.noreply.github.com>
2021-04-03 15:12:34 +0200

    Formatting (#2)

--- src/connector.rs
+++ src/connector.rs
@@ -1,21 +1,27 @@
-use tokio::{net::UdpSocket, sync::mpsc::{UnboundedReceiver, UnboundedSender}};
-use tokio_util::{udp::UdpFramed};
-use futures::{FutureExt, SinkExt, select, stream::StreamExt};
 use std::net::SocketAddr;
 
-use crate::{rpc::{RpcMessage, RpcCodec}};
+use futures::{select, stream::StreamExt, FutureExt, SinkExt};
+use tokio::{
+    net::UdpSocket,
+    sync::mpsc::{UnboundedReceiver, UnboundedSender},
+};
+use tokio_util::udp::UdpFramed;
+
+use crate::rpc::{RpcCodec, RpcMessage};
 
 pub type ConnectorReceiver = UnboundedReceiver<(RpcMessage, SocketAddr)>;
 pub type ConnectorSender = UnboundedSender<(RpcMessage, SocketAddr)>;
 
 pub fn new_connector(addr: SocketAddr) -> (ConnectorSender, ConnectorReceiver) {
-    let (outbound_tx, outbound_rx) = tokio::sync::mpsc::unbounded_channel::<(RpcMessage, SocketAddr)>();
-    let (inbound_tx, mut inbound_rx) = tokio::sync::mpsc::unbounded_channel::<(RpcMessage, SocketAddr)>();
+    let (outbound_tx, outbound_rx) =
+        tokio::sync::mpsc::unbounded_channel::<(RpcMessage, SocketAddr)>();
+    let (inbound_tx, mut inbound_rx) =
+        tokio::sync::mpsc::unbounded_channel::<(RpcMessage, SocketAddr)>();
     tokio::spawn(async move {
         let socket = UdpSocket::bind(&addr).await.unwrap();
         let framed = UdpFramed::new(socket, RpcCodec::new());
         let (mut udp_tx, mut udp_rx) = framed.split();
-        loop {          
+        loop {
             select! {
                 res = udp_rx.next().fuse() => {
                     println!("Socket recv: {:?}", res);
@@ -31,7 +37,7 @@ pub fn new_connector(addr: SocketAddr) -> (ConnectorSender, ConnectorReceiver) {
                             break
                         }
                     }
-                }, 
+                },
                 res = inbound_rx.recv().fuse() => {
                     println!("rx recv: {:?}", res);
                     match res {
@@ -45,7 +51,7 @@ pub fn new_connector(addr: SocketAddr) -> (ConnectorSender, ConnectorReceiver) {
                             println!("Inbound channel closed!");
                             break
                         }
-                    }  
+                    }
                 }
             }
         }
@@ -55,14 +61,13 @@ pub fn new_connector(addr: SocketAddr) -> (ConnectorSender, ConnectorReceiver) {
     (inbound_tx, outbound_rx)
 }
 
-
 #[cfg(test)]
 mod tests {
     use super::*;
     use futures::SinkExt;
     use std::net::SocketAddr;
     use std::time::Duration;
-    use tokio::{time, net::UdpSocket};
+    use tokio::{net::UdpSocket, time};
     use tokio_stream::StreamExt;
     use tokio_util::udp::UdpFramed;
 
@@ -83,7 +88,10 @@ mod tests {
         Ok(())
     }
 
-    async fn write_into_socket(socket: &mut UdpFramed<RpcCodec>, addr: SocketAddr) -> Result<(), ()> {
+    async fn write_into_socket(
+        socket: &mut UdpFramed<RpcCodec>,
+        addr: SocketAddr,
+    ) -> Result<(), ()> {
         for _ in 0..4usize {
             let msg = select_msg();
             println!("Sending {:?} into socket", msg);
@@ -99,7 +107,7 @@ mod tests {
             tx.send((msg, addr)).unwrap();
         }
         Ok(())
-    } 
+    }
 
     async fn received_from_connector(mut rx: ConnectorReceiver) -> Result<(), ()> {
         let timeout = Duration::from_millis(200);
@@ -107,25 +115,24 @@ mod tests {
             println!("[connector] recv: {:?} {:?}", addr, req);
         }
         Ok(())
-    } 
+    }
 
     #[tokio::test]
     async fn test_send_to_connector() {
         let addr: SocketAddr = "127.0.0.1:0".parse().unwrap();
 
         let (a_tx, _) = new_connector(addr);
-    
+
         let b = UdpSocket::bind(&addr).await.unwrap();
         let b_addr = b.local_addr().unwrap();
 
-        
         let mut b = UdpFramed::new(b, RpcCodec::new());
         let read = read_from_socket(&mut b);
 
         let send = send_to_connector(a_tx, b_addr);
         // Start off by sending a ping from a to b, afterwards we just print out
         // what they send us and continually send pings
-    
+
         // Run both futures simultaneously of `a` and `b` sending messages back and forth.
         match tokio::try_join!(read, send) {
             Err(e) => println!("an error occurred; error = {:?}", e),
@@ -139,34 +146,34 @@ mod tests {
         let (_a_tx, a_rx) = new_connector(a_addr);
 
         let b = UdpSocket::bind("127.0.0.1:0").await.unwrap();
-        
+
         let mut b = UdpFramed::new(b, RpcCodec::new());
-    
+
         let write = write_into_socket(&mut b, a_addr);
 
         let recv = received_from_connector(a_rx);
         // Start off by sending a ping from a to b, afterwards we just print out
         // what they send us and continually send pings
-    
+
         // Run both futures simultaneously of `a` and `b` sending messages back and forth.
         match tokio::try_join!(recv, write) {
             Err(e) => println!("an error occurred; error = {:?}", e),
             _ => println!("done!"),
         }
-    }    
-    
+    }
+
     #[tokio::test]
     async fn all() {
         let c_addr: SocketAddr = "127.0.0.1:5556".parse().unwrap();
 
         let a = UdpSocket::bind("127.0.0.1:0").await.unwrap();
-        let a_addr = a.local_addr().unwrap();    
+        let a_addr = a.local_addr().unwrap();
         let mut a = UdpFramed::new(a, RpcCodec::new());
         let read_socket = read_from_socket(&mut a);
-        
+
         let b = UdpSocket::bind("127.0.0.1:0").await.unwrap();
         let mut b = UdpFramed::new(b, RpcCodec::new());
-    
+
         let write_socket = write_into_socket(&mut b, c_addr);
 
         let (c_tx, c_rx) = new_connector(c_addr);
